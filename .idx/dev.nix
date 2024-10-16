97dbb5b21805cd65b4e410b97ff3a48c09a93c80
zoobliminal <zoobliminal@gmail.com>
2024-10-16 06:33:40 +0000

    Ajustes en requirements 	modified:   .idx/dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -9,8 +9,8 @@
     pkgs.go
     pkgs.python311
     pkgs.python311Packages.pip
-    #pkgs.nodejs_20
-    #pkgs.nodePackages.nodemon
+    pkgs.nodejs_20
+    pkgs.nodePackages.nodemon
     pkgs.python311Packages.django
     pkgs.sudo-rs
     pkgs.sudo
