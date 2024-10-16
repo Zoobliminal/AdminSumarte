e25c83a26c45693dcd257248fd5e2f2de7da1681
zoobliminal <zoobliminal@gmail.com>
2024-10-16 12:57:43 +0000

    contadores

--- Contadores/urls.py
+++ Contadores/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.contadores, name="contadores"),
+    path('registrar_cambio_contador/', views.registrar_cambio_contador, name='registrar_cambio_contador'),
     path('cambios_contador/', views.cambios_contador, name="cambios_contador"),
     path('lecturas_abonados/', views.lecturas_abonados, name="lecturas_abonados"),
     
--- Contadores/views.py
+++ Contadores/views.py
@@ -17,3 +17,17 @@ def lecturas_abonados(request):
     Lecturas=LecturaAbonado.objects.all()
     return render (request, "Contadores/lecturas_abonados.html",{"lecturas":Lecturas})
 
+def registrar_cambio_contador(request):
+    if request.method == 'POST':
+        #form = ControlDiarioForm(request.POST)
+        if form.is_valid():
+            # Guardar el nuevo control diario en la base de datos
+            form.save()
+            # Redirigir a la lista de controles diarios después de crear uno nuevo
+            return redirect(reverse('Contadores/cambios_contador.html'))  # Cambia el nombre a la vista que maneja la lista
+    else:
+        form = ControlDiarioForm()
+
+    controles = ControlDiario.objects.all().order_by('-año', '-mes')    
+    return render(request, 'Contadores/registrar_cambio_contador.html', {'form': form})
+
